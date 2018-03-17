50ab8dafe2b44bf1790a657d6e8996d0d26f8a4a
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-03-17 08:25:45 +0700

    Add basic UI

--- app/src/main/java/com/codeko/userclone/MainActivity.java
+++ app/src/main/java/com/codeko/userclone/MainActivity.java
@@ -2,6 +2,11 @@ package com.codeko.userclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import com.parse.ParseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    public void getStarted(View view) {
+        Switch userTypeSwitch = findViewById(R.id.userTypeSwitch);
+        Log.i("Switch value", String.valueOf(userTypeSwitch.isChecked()));
     }
 }
